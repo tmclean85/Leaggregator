226fa1f87b3a6d23167d533eaa973d48b005d64c
tmclean85 <trevor.a.mclean@gmail.com>
2019-05-02 12:34:27 -0700

    Fetch data, conditionally render loader gif based on data state

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Results from '../Results/Results';
 import './styles.css';
 
 class Input extends Component {
@@ -7,12 +8,24 @@ class Input extends Component {
     super(props);
 
     this.state = {
-      summoner: ''
+      summoner: '',
+      data: null,
+      isLoading: false
     }
   }
 
   onChange = (event) => {
     this.setState({ summoner: event.target.value });
+  }
+
+  onClick = (event) => {
+    event.preventDefault();
+    console.log('clicked', this.state.summoner);
+    this.setState({ isLoading: true, data: null});
+    fetch(`http://localhost:8000?summoner=${this.state.summoner}`)
+    .then(result => { return result.json() })
+    .then(res => { this.setState({ data: res }) })
+    .then(() => this.setState({ isLoading: false}));
   }
 
   render() {
@@ -22,6 +35,11 @@ class Input extends Component {
           type="text"
           value={this.state.value}
           onChange={this.onChange}
+        />
+        <button onClick={this.onClick}>Submit</button>
+        <Results 
+          isLoading={this.state.isLoading}
+          data={this.state.data}
         />
       </div>
     )
