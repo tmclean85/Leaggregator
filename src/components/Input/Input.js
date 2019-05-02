0166bdd930a8bf89b05482d0649e61a328c52030
tmclean85 <trevor.a.mclean@gmail.com>
2019-05-02 10:49:29 -0700

    Build initial project structure, implement router

---
+++ src/components/Input/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Input Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Input;
---
+++ src/components/Results/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Results Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Results;
